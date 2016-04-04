c64b33d4fa85cab65c4e2918de8b7f8b1bb94a9f
Matheus Sampaio <matheus1401@gmail.com>
2016-04-04 17:54:36 -0500

    fix: removing user from api

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,12 +3,10 @@ const router = express.Router();
 
 const Music = require('../models/music');
 const AudioData = require('../models/audio-data');
-const User = require('../models/user');
 
 module.exports = function (app) {
   Music.register(app, '/api/music');
   AudioData.register(app, '/api/audiodata');
-  User.register(app, '/api/user');
 
   return router;
 };
